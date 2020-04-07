f50ffd1823b2e5b999a38c48036e12c63f16fa78
kumarwyh <kumarwyh@amazon.com>
2020-04-07 19:35:13 +0530

    Updated CodeBase

--- src/main/java/com/example/hello/HelloApplication.java
+++ src/main/java/com/example/hello/HelloApplication.java
@@ -2,9 +2,11 @@ package com.example.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-public class HelloApplication {
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class HelloApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
